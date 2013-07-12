dc1f2b153cfa823fb93f114661830bb312f6abe3
Vasily Vlasov <vasily.vlasov@gmail.com>
2013-07-12 11:30:17 +0400

    Added some mapping related test

--- src/test/java/com/othelle/jtuples/MapUtilsTest.java
+++ src/test/java/com/othelle/jtuples/MapUtilsTest.java
@@ -3,10 +3,7 @@ package com.othelle.jtuples;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.othelle.jtuples.Tuples.tuple;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,11 +14,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class MapUtilsTest {
 
     @Test
+    public void mapTuple2Ordered() {
+        Map<Integer, String> map = MapUtils.map(Arrays.asList(tuple(25, "1"), tuple(1, "value2"), tuple(3, "value2")), true);
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            assertThat(Tuples.convert(entry), Matchers.equalTo(tuple(25, "1")));
+            break;
+        }
+
+        assertThat(map.entrySet(), Matchers.hasSize(3));
+        assertThat(map.get(1), Matchers.equalTo("value2"));
+    }
+
+
+    @Test
     public void testMapPutRetrieve() {
-        Map<Integer, Tuple2<String, String>> map = MapUtils.map3(createListOfTuples(10));
-
-        assertThat(map.get(1), Matchers.equalTo(tuple("1", "value2")));
-
+        Map<Integer, Tuple2<String, String>> map = MapUtils.map3(Arrays.asList(tuple(1, "1", "value1"), tuple(1, "2", "value2")));
+        assertThat(map.entrySet(), Matchers.hasSize(2));
+        assertThat(map.get(1), Matchers.equalTo(tuple("1", "value1")));
     }
 
     private Collection<Tuple3<Integer, String, String>> createListOfTuples(int number) {
@@ -32,6 +42,22 @@ public class MapUtilsTest {
 
         }
         return result;
+    }
+
+
+    @Test
+    public void flattenTest() {
+        Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+        map.put("key1", 1);
+        map.put("key2", 2);
+
+        List<Tuple2<String, Integer>> list = MapUtils.flatten(map);
+
+        assertThat(list, Matchers.hasSize(2));
+        assertThat(list.get(0), Matchers.equalTo(tuple("key1", 1)));
+        assertThat(list.get(1), Matchers.equalTo(tuple("key2", 2)));
+
+
     }
 
 }
